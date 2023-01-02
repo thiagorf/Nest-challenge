20c771f0f3a77e0833051047fc763b2155a00ad7
thiagorf <trfarciroli@gmail.com>
2023-01-02 16:52:08 -0300

    nestjs-swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './provider/prisma/prisma.service';
 import * as session from 'express-session';
@@ -8,10 +9,23 @@ import Redis from 'ioredis';
 const RedisStore = connectRedis(session);
 
 // add environment variable
-const redisClient = new Redis('redis://redis:6379');
+const redisClient = new Redis('redis://localhost:6379');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Finances API')
+    .setDescription('finances api interview challenge')
+    .setVersion('1.0')
+    .addTag('finances')
+    .addCookieAuth('optional-session-id')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   app.use(
     session({
       store: new RedisStore({
